758f1a3c6cc2220d88c30fa60c80ce903ca2d202
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-06 19:44:31 +0900

    finished part 2

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users', function () {
+    return 'Hi Users';
+});
+
+Route::get('/users/{name?}', function ($name = null) {
+    return 'Hi Users' . $name;
+})->where('name', '[a-zA-Z]+');
+
+Route::get('/products/{id?}', function ($id = null) {
+    return 'Product id is ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('/middelware-id-test/{id?}', function ($id = null) {
+    return 'Product id is ' . $id;
+});
+
+Route::match(['get', 'post'], '/students', function (Request $request) {
+    return 'Requested method is ' . $request->method();
+});
+
+Route::any('/posts', function (Request $request) {
+    return 'Post only . Requested method is ' . $request->method();
+});
