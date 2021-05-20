7122b92bde4dd0c5a97d9f01abf64fcb9ca5afb3
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-20 18:04:14 +0900

    10:00

--- resources/views/add-post.blade.php
+++ resources/views/add-post.blade.php
@@ -9,35 +9,38 @@
 </head>
 
 <body>
-    <section>
+
+    <section style="padding-top:60px;">
         <div class="container">
             <div class="row">
                 <div class="col-md-6 offset-md-3">
                     <div class="card">
                         <div class="card-header">
-                            Add new Post
+                            Add Post
                         </div>
                         <div class="card-body">
-                            @if(Session::has('post_created'))
-                            <div class="alert alert-success" role="alert">
-                                {{Session::get('post_created')}}
-                            </div>
-                            @endif
-                            <form action="{{route('post.addsubmit')}}" method="POST">
+                            <form action="" method="POST">
                                 @csrf
-                                <label for="title">Post Title</label>
-                                <input type="text" name="title" class="form-control" placeholder="Enter Post Title">
+                                <div class="form-group">
+                                    <label for="title">Post Title</label>
+                                    <input type="text" name="title" id="" class="form-control" placeholder="Enter Post Title">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="body">Article</label>
+                                    <input type="text" name="body" id="" class="form-control" placeholder="Enter Post Article">
+                                </div>
+
+                                <button type="submit">Add Post</button>
+
+                            </form>
                         </div>
-                        <div class="form-group">
-                            <label for="body">Post Description</label>
-                            <textarea name="body" id="" class="form-control" cols="30" rows="8"></textarea>
-                        </div>
-                        <input type="submit" value="Submit">
-                        </form>
                     </div>
+
                 </div>
             </div>
         </div>
+    
     </section>
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -25,48 +25,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [ProductController::class, 'index'])
-    ->name('product.index');
-
-// Route::get('/home', [HomeController::class, 'index'])
-//     ->name('home.index');
-
-Route::get('/home/{name?}', [HomeController::class, 'index'])
-    ->name('home.index');
-
-Route::get('/user', [UserController::class, 'index'])
-    ->name('user.index');
-
-Route::get('/posts', [ClientController::class, 'getAllPost'])
-    ->name('posts.getallpost');
-
-Route::get('/posts/{id}', [ClientController::class, 'getPostById'])
-    ->name('posts.getpostbyid');
-
-Route::get('/add-post', [ClientController::class, 'addPost'])->name('posts.addpost');
-
-Route::get('/update-post', [ClientController::class, 'updatePost'])->name('posts.update');
-
-Route::get('/delete-post/{id}', [ClientController::class, 'deletePost'])->name('post.delete');
-
-Route::get('fluent-string', [FluentController::class, 'index'])
-    ->name('fluent.index');
-
-Route::get('/login', [LoginController::class, 'index'])
-    ->name('login.index')
-    ->middleware('checkuser');
-
-Route::post('/login', [LoginController::class, 'loginSubmit'])
-    ->name('login.submit');
-
-Route::get('/session/get', [SessionController::class, 'getSessionData'])->name('session.get');
-
-Route::get('/session/set', [SessionController::class, 'storeSessionData'])->name('session.store');
-
-Route::get('/session/remove', [SessionController::class, 'deleteSessionData'])->name('session.delete');
-
-Route::get('/posts', [PostController::class, 'getAllPost'])->name('post.getallpost');
-
-Route::get('/add-post', [PostController::class, 'addPost'])->name('post.add');
-
-Route::post('/add-post', [PostController::class, 'addPostSubmit'])->name('post.addsubmit');
+Route::get('/add-post', [PostController::class, 'addPost']);
